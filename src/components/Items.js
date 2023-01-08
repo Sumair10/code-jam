4ca22a49c3c823ede28a2c51badde8ebfb0dc2c4
Sumair10 <sumairzahid123@gmail.com>
2023-01-08 12:04:58 +0500

    finalize

--- src/components/Items.js
+++ src/components/Items.js
@@ -179,7 +179,7 @@ export default function MediaCard() {
                         // onClick={() => addProduct(product)}
                         sx={{
                           border: "2px solid #ff6838",
-                          // borderRadius: 10,
+                          borderRadius: 10,
                           fontSize: 12,
                           p: 1,
                           fontWeight: "bold",
